943d8a09224597e8c1bbd4c72213f067a0131cc7
Gaurav Jain <gauravjain0781@gmail.com>
2023-10-09 23:15:36 +0530

    Initialized Menu Project

---
+++ menu/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Categories from "./Categories";
+import Menu from "./Menu";
+import items from "./data.js";
+
+function App() {
+  return 
+}
+
+export default App;
