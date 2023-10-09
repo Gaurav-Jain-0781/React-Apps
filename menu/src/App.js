b1cc4e5146b37c90eef9ad35e370f1c8f8792a43
Gaurav Jain <gauravjain0781@gmail.com>
2023-10-09 23:56:53 +0530

    Menu Component Done

--- menu/src/App.js
+++ menu/src/App.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Categories from "./Categories";
 import Menu from "./Menu";
 import items from "./data.js";
 
 function App() {
-  return 
+  const [menuItem, setMenuItem] = useState(items);
+  const [categories, setCategories] = useState([]); 
+ 
+  return(
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h4>Our Menu</h4>
+          <div className="underline"></div>
+        </div>        
+        <Categories/>
+        <Menu items={menuItem}/>
+      </section>
+    </main>
+  ) 
 }
 
 export default App;
--- menu/src/Menu.js
+++ menu/src/Menu.js
@@ -1,9 +1,20 @@
 import React from 'react'
 
-const Menu = () => {
+const Menu = ({items}) => {
   return (
-    <div>
-      
+    <div className='section-center'>
+        {items.map((i) => {
+            return <article key={i.id} className="menu-item">
+                <img src={i.img} alt={i.title} className="photo"/>
+                <div className='item-info'>
+                    <header>
+                        <h4>{i.title}</h4>
+                        <h4 classname="price">${i.price}</h4>
+                    </header>
+                    <p className='item-text'>{i.desc}</p>
+                </div>
+            </article>
+        })}
     </div>
   )
 }
