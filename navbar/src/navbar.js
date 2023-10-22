e9be9ced0662c7b58c4d6accf7edaea85a837c66
Gaurav Jain <gauravjain0781@gmail.com>
2023-10-23 00:25:32 +0530

    UseRef Functionality Added

--- navbar/src/navbar.js
+++ navbar/src/navbar.js
@@ -1,10 +1,23 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import logo from './logo.jpg'
 import { FaBars } from 'react-icons/fa'
 import { links, socials } from './data.js'
 
 const Navbar = () => {
     const [showLinks, setshowLinks] = useState(false)
+    const linksContainerRef = useRef(null)
+    const linksRef = useRef(null)
+
+    useEffect(()=> {
+        const linkheight = linksRef.current.getBoundingClientRect().height
+        if(showLinks){
+            linksContainerRef.current.style.height = `${linkheight}px`
+        }
+        else{
+            linksContainerRef.current.style.height = '0px'
+        }
+        console.log(linkheight)
+    }, [showLinks])
 
   return <nav>
       <div className='nav-center'>
@@ -15,8 +28,8 @@ const Navbar = () => {
             </button>
         </div>
 
-        <div className='show-container links-container'>
-            <ul className='links'>
+        <div className='links-container' ref={linksContainerRef}>
+            <ul className='links' ref={linksRef}>
                 {links.map((link) => {
                     return <li key={link.id}><a href={link.url}> {link.text} </a></li>
                 })}
